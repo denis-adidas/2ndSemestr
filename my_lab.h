15224826cc123821129d30f8fe5dc65926a57e32
denis_adidas <veb-kamera2013@ya.ru>
2022-02-28 12:07:58 +0300

    Create my_lab.h

---
+++ my_lab.h
@@ -0,0 +1,10 @@
+//
+// Created by Денис Адидас on 2/18/22.
+//
+#ifndef my_lab
+#define my_lab
+void ltoae(long num, char str[]);
+long my_strtol(char* str, char** pEnd, int ss);
+int isSymb(char ch);
+
+#endif
