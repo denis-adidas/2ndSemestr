386f71517f48b8d776045d7a2f969c5fd4ca8410
denis_adidas <45899852+denis-adidas@users.noreply.github.com>
2022-03-09 19:13:46 +0300

    Update my_lab.cpp

--- lab7/my_lab.cpp
+++ lab7/my_lab.cpp
@@ -1,38 +1,34 @@
-//
-// Created by Денис Адидас on 2/18/22.
-//
-
 #include <algorithm>
 #include <math.h>
 #include "my_lab.h"
 
 void ltoae(long num, char str[]) {
-        int i = 0, flag = 0;
-        if (num < 0) {
-            flag = 1;
-            num = abs(num);
-            i = 1;
-            str[0] = '-';
-        }
-        while (num != 0) {
-            int d = 0;
-            d = num % 11;
-            num /= 11;
+    int i = 0, flag = 0;
+    if (num < 0) {
+        flag = 1;
+        num = abs(num);
+        i = 1;
+        str[0] = '-';
+    }
+    while (num != 0) {
+        int d = 0;
+        d = num % 11;
+        num /= 11;
 //            printf("Test: %d \n", num);
-            if (d == 10) {
-                str[i] = 'A';
-            }
-            else {
-                str[i] = '0'+d;
-            }
-            i++;
-        }
-        if (flag == 1) {
-            std::reverse(&str[1], &str[i]);
+        if (d == 10) {
+            str[i] = 'A';
         }
         else {
-            std::reverse(&str[0], &str[i]);
+            str[i] = '0'+d;
         }
+        i++;
+    }
+    if (flag == 1) {
+        std::reverse(&str[1], &str[i]);
+    }
+    else {
+        std::reverse(&str[0], &str[i]);
+    }
 
 }
 int isSymb(char ch) {
@@ -47,6 +43,28 @@ int isNumber(char ch) {
     if ((ch >= '0') && (ch <= '9')) return 1;
     else return 0;
 }
+int isSmallLetter(char ch) {
+    if ((ch >= 'a') && (ch <= 'z')) return 1;
+    else return 0;
+}
+char* my_string() {
+    int str_idx = 0, len_str = 20;
+    char* str = (char*) malloc(sizeof(char) * len_str);
+    if (str == NULL) exit(-1);
+    char a = ' ';
+    while ((a = getchar()) !='\n') {
+        if (str_idx >= len_str) {
+            char* str_temp = (char*)(realloc(str, len_str *= 2));
+            if (str_temp == NULL) {
+                free(str);
+                exit(-1);
+            }
+            str = str_temp;
+        }
+        str[str_idx++] = a;
+    }
+    return &str[0];
+}
 long my_strtol(char* str, char** pEnd, int ss) {
     int count = strlen(str);
     int result = 0;
@@ -55,17 +73,20 @@ long my_strtol(char* str, char** pEnd, int ss) {
     for (i; i < count; i++) {
         int exam_l = (int)(str[i] - 'A');
         int exam_n = (int)(str[i] - '0');
+        int exam_sl = (int)(str[i] - 'a');
 
         if ((isSymb(str[i])) && (exam_l < ss) && (exam_l >= 0)) {
-            result += (((int)(str[i] - 'A')) + 10) * pow(ss, count - i - 1);
+            result += (exam_l + 10) * pow(ss, count - i - 1);
         }
         else if ((isNumber(str[i])) && (exam_n < ss) && (exam_n >= 0)) {
-            result += ((int)(str[i]) - '0') * pow(ss, count - i - 1);
+            result += exam_n * pow(ss, count - i - 1);
+        }
+        else if ((isSmallLetter(str[i])) && (exam_sl < ss) && (exam_sl >= 0)) {
+            result += (exam_sl + 10) * pow(ss, count - i - 1);
         }
         else {
             *pEnd = &str[i];
-            printf("Error: symbol not belong notation \n");
-            return 0;
+            return result;
         }
     }
     if (str[0] != '-') return result;
