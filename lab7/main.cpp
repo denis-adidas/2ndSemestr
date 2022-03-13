e64d00784438339d8964f9d86f52a698475e616a
denis_adidas <veb-kamera2013@ya.ru>
2022-03-13 16:33:28 +0300

    Update main.cpp

--- lab7/main.cpp
+++ lab7/main.cpp
@@ -1,13 +1,17 @@
+//
+// Created by Денис Адидас on 3/13/22.
+//
+
 #include <iostream>
 #include <stdlib.h>
-#include "my_lab.h"
+#include "my_lib.h"
 
 int main() {
     char* str = my_string();
     char* pEnd;
     char* pEnd2;
-    long c = strtol(str, &pEnd, 36);
-    long d = my_strtol(str, &pEnd2, 36);
+    long c = strtol(str, &pEnd, 17);
+    long d = my_strtol(str, &pEnd2, 17);
 
     printf("Test0: %d\n", c);
     printf("Test1: %d\n", d);
