0a0d70302bd2a03114e2092d215e9afbbfbf5932
denis_adidas <veb-kamera2013@ya.ru>
2022-03-13 16:33:20 +0300

    Create my_lib.h

---
+++ lab7/my_lib.h
@@ -0,0 +1,13 @@
+//
+// Created by Денис Адидас on 3/13/22.
+//
+
+
+#ifndef my_lab
+#define my_lab
+void ltoae(long num, char str[]);
+long my_strtol(char* str, char** pEnd, int ss);
+int isSymb(char ch);
+char* my_string();
+
+#endif
