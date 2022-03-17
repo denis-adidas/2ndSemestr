5e676f2afb9755d838934882097f49f058757af3
denis_adidas <veb-kamera2013@ya.ru>
2022-03-17 19:26:44 +0300

    Create my_lib.h

---
+++ lab8/my_lib.h
@@ -0,0 +1,13 @@
+//
+// Created by Денис Адидас on 3/13/22.
+//
+
+
+#ifndef my_lib
+#define my_lib
+void ltoae(long num, char str[]);
+long my_strtol(char* str, char** pEnd, int ss);
+char* my_string();
+void* resize(void* mas);
+
+#endif
