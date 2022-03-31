7bfbc58ba0e2ab434f7d0fd5530cdbb05c856c6e
denis_adidas <veb-kamera2013@ya.ru>
2022-03-31 21:30:21 +0300

    Update my_lib.h

--- lab7/my_lib.h
+++ lab7/my_lib.h
@@ -3,11 +3,12 @@
 //
 
 
-#ifndef my_lab
-#define my_lab
+#ifndef my_lib
+#define my_lib
 void ltoae(long num, char str[]);
 long my_strtol(char* str, char** pEnd, int ss);
 int isSymb(char ch);
 char* my_string();
+char* fromIntToChar(int num);
 
 #endif
