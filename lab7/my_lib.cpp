bb6c2ab6e14dfd262337de2108f220e6322d5272
denis_adidas <veb-kamera2013@ya.ru>
2022-03-31 21:30:26 +0300

    Update my_lib.cpp

--- lab7/my_lib.cpp
+++ lab7/my_lib.cpp
@@ -43,22 +43,22 @@ int my_pow(int ss, int count) {
     }
     return a;
 }
-int isSymb(char ch) {
-    if (ch >= 'A' && ch <= 'Z') {
-        return 1;
+
+int charToNum(char symb) {
+    if (symb >= '0' && symb <= '9') {
+        return int(symb - '0');
     }
-    else {
-        return 0;
+    if (symb >= 'a' && symb <= 'z') {
+        return int(10 + symb - 'a');
     }
+    if (symb >= 'A' && symb <= 'Z') {
+        return int(10 + symb - 'A');
+    }
+    return -1;
+
+
 }
-int isNumber(char ch) {
-    if ((ch >= '0') && (ch <= '9')) return 1;
-    else return 0;
-}
-int isSmallLetter(char ch) {
-    if ((ch >= 'a') && (ch <= 'z')) return 1;
-    else return 0;
-}
+
 char* my_string() {
     int str_idx = 0, len_str = 20;
     char* str = (char*) malloc(sizeof(char) * len_str);
@@ -77,30 +77,72 @@ char* my_string() {
     }
     return &str[0];
 }
+void* resize(void* mas) {
+    int mas_len = 20;
+    void* mas_temp = realloc(mas, mas_len *= 2);
+    if (mas_temp == NULL) {
+        free(mas);
+        exit(-1);
+    }
+    mas = mas_temp;
+    free(mas_temp);
+}
+
+char* fromIntToChar(int num) {
+    int lenstr = 20;
+    char* str = (char*)malloc(sizeof(int) * lenstr);
+    int i = 0;
+    while (num > 0) {
+        if (i == lenstr) {
+            resize(str);
+        }
+        str[i] = num % 10 + '0';
+        i++;
+        num /= 10;
+    }
+    char* str_temp = (char*)malloc(sizeof(int) * strlen(str) + 2);
+    i = strlen(str) - 1;
+    for (int j = 0; j < strlen(str); j++) {
+        str_temp[j] = str[i];
+        i--;
+    }
+    free(str);
+    return str_temp;
+}
 long fromCharToLong(char i, int ss, int count) {
-    int exam_l = (int)(i - 'A');
-    int exam_n = (int)(i - '0');
-    int exam_sl = (int)(i - 'a');
     int result = 0;
-    if ((isSymb(i)) && (exam_l < ss) && (exam_l >= 0)) {
-        result += (exam_l + 10) * my_pow(ss, count);
-    }
-    else if ((isNumber(i)) && (exam_n < ss) && (exam_n >= 0)) {
-        result += exam_n * my_pow(ss, count);
-    }
-    else if ((isSmallLetter(i)) && (exam_sl < ss) && (exam_sl >= 0)) {
-        result += (exam_sl + 10) * my_pow(ss, count);
+    int ch = charToNum(i);
+    if ((ch <= ss) && (ch >= 0)) {
+        result += ch * my_pow(ss, count);
     }
     else {
         result = -1;
     }
     return result;
 }
+
+
 long my_strtol(char* str, char** pEnd, int ss) {
     int count = strlen(str);
     int result = 0;
-    int i = 0, c = 0;
+    int i = 0;
     int flag = 0;
+    if ((str[0] == '0') && (str[1] == 'x')) {
+        count = strlen(str);
+        for (i = 2; i < count; i++) {
+            if (fromCharToLong(str[i], 16, count - i - 1) != -1) {
+                result += fromCharToLong(str[i], ss, count - i - 1);
+            }
+            else {
+                result = 0;
+                *pEnd = &str[i];
+                flag = 1;
+                break;
+            }
+        }
+        str = fromIntToChar(result);
+    }
+
     if (str[0] == '-') i = 1;
     for (i ; i < count; i++) {
         if (fromCharToLong(str[i], ss, count - i - 1) != -1) {
@@ -112,7 +154,7 @@ long my_strtol(char* str, char** pEnd, int ss) {
             flag = 1;
             break;
         }
-    }
+    } //если 10-я
     if (flag == 1) {
         count = i;
         if (str[0] == '-') i = 1;
@@ -126,8 +168,11 @@ long my_strtol(char* str, char** pEnd, int ss) {
                 break;
             }
         }
-    }
+    } //проверка после ошибки
 
     if (str[0] != '-') return result;
     else return -result;
 }
+
+//0x32432 - 16-й
+//0235252 - 8-й
