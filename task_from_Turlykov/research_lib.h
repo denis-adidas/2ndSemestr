26e4f55c2b48bd05e53b11e767e10d74676f0fbf
denis_adidas <veb-kamera2013@ya.ru>
2022-05-12 23:06:57 +0300

    Create research_lib.h

---
+++ task_from_Turlykov/research_lib.h
@@ -0,0 +1,17 @@
+//
+// Created by Денис Адидас on 3/13/22.
+//
+
+
+#ifndef my_lib
+#define my_lib
+void ltoae(long num, char str[]);
+long my_strtol(char* str, char** pEnd, int ss);
+char* my_string();
+void* resize(void* mas);
+float* masFloat(char* str);
+int mas_len(float* mas);
+int my_pow(int ss, int count);
+int min(float* mas);
+
+#endif
