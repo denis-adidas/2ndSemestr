6ec5f7093008ebe5d2472d92ed401d45354a493f
denis_adidas <veb-kamera2013@ya.ru>
2022-05-13 18:39:40 +0300

    Update main.cpp

--- task_from_Turlykov/main.cpp
+++ task_from_Turlykov/main.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 #include "research_lib.h"
+#include "math.h"
 
 int main() {
     int n = 0;
     printf("Enter count of graphs: \n");
     scanf("%d", &n);
-    float** matrix = (float**)malloc(sizeof(float**) * (n) + 1);
+    int** matrix = (int**)malloc(sizeof(int**) * (n) + 1);
     char* str;
 
-    printf("Enter numbers of row #0\n");
-    matrix[0] = (float*)malloc(sizeof(float*) + 1);
+    printf("Enter numbers of row #1\n");
+    matrix[0] = (int*)malloc(sizeof(int*) + 1);
     getchar();
     str = my_string();
-    float* mas_float = masFloat(str);
+    int* mas_float = masFloat(str);
     matrix[0] = mas_float;
     for (int i = 1; i < n; i++) {
         printf("Enter numbers of row #%d\n", i + 1);
-        matrix[i] = (float*)malloc(sizeof(float*) + 1);
+        matrix[i] = (int*)malloc(sizeof(int*) + 1);
         str = my_string();
         mas_float = masFloat(str);
         matrix[i] = mas_float;
@@ -25,16 +26,18 @@ int main() {
     int j = 0;
     int ctrl_flag_row = 0;
     int max_sum_in_row = 0;
-    int* ctrl_flag_clm = (int*)malloc(sizeof(int) * 4);
+    int* ctrl_flag_clm = (int*)calloc(n, sizeof(int));
     while (j < n) {
         int i = 0;
         float sum_str = 0;
         while (i < n) {
             sum_str += matrix[j][i];
+//            printf("test 0: %d \n", matrix[j][i]);
             ctrl_flag_clm[i] += matrix[j][i];
+
             i++;
         }
-        if (sum_str > 2)
+        if (sum_str > (int)(n/2))
             ctrl_flag_row++;
         j++;
     }
@@ -42,7 +45,7 @@ int main() {
     int graph_ctrl_flag = 0;
     for (int i = 0; i < n; i++) {
 //        printf("test3 %d\n", ctrl_flag_clm[i]);
-        if (ctrl_flag_row >= 2 && ctrl_flag_clm[i] >= 2) graph_ctrl_flag = 1;
+        if (ctrl_flag_row >= (int)(n/2) && ctrl_flag_clm[i] >= (int)(n/2)) graph_ctrl_flag = 1;
         else {
             graph_ctrl_flag = 0;
             break;
@@ -51,3 +54,12 @@ int main() {
     if (graph_ctrl_flag > 0) printf("Graph is connected\n");
     else printf("Graph is not connected\n");
 }
+/*
+ * 1 1 0
+Enter numbers of row #2
+1 1 1
+Enter numbers of row #3
+0 1 1
+
+ сделать переборочный алгоритм
+ */
