1fbb3bb44ff39c62a452c9aff7f700e3c076d6da
denis_adidas <veb-kamera2013@ya.ru>
2022-05-19 23:44:07 +0300

    Create lab_polinom_C.h

---
+++ lab_polinom_c/lab_polinom_C.h
@@ -0,0 +1,28 @@
+//
+// Created by Денис Адидас on 5/18/22.
+//
+
+#ifndef LAB_POLINOM_C_LAB_POLINOM_C_H
+#define LAB_POLINOM_C_LAB_POLINOM_C_H
+#include <iostream>;
+
+class polinom {
+protected:
+    int degree;
+    int* coef;
+public:
+polinom(){};
+polinom(int count);
+//~polinom(){delete[] coef; }
+polinom operator/(polinom&);
+polinom operator%(polinom&);
+polinom operator*(polinom&);
+polinom max_p(polinom& ob_1, polinom& ob_2);
+//polinom& operator=(polinom&);
+int get_degr() { return degree;}
+int push(int, int*);
+static polinom NOD(polinom&, polinom&);
+static polinom NOK(polinom&, polinom&);
+friend std::ostream &operator<<(std::ostream &, polinom&);
+};
+#endif //LAB_POLINOM_C_LAB_POLINOM_C_H
