53fe4ad9939011dfff7ccc062f38df3d4adef180
denis_adidas <veb-kamera2013@ya.ru>
2022-05-22 14:53:30 +0300

    Update lab_polinom_C.h

--- lab_polinom_c/lab_polinom_C.h
+++ lab_polinom_c/lab_polinom_C.h
@@ -18,11 +18,11 @@ polinom operator/(polinom&);
 polinom operator%(polinom&);
 polinom operator*(polinom&);
 polinom max_p(polinom& ob_1, polinom& ob_2);
-//polinom& operator=(polinom&);
+//polinom operator=(polinom t);
 int get_degr() { return degree;}
 int push(int, int*);
-static polinom NOD(polinom&, polinom&);
-static polinom NOK(polinom&, polinom&);
+static polinom& NOD(const polinom&, const polinom&);
+static polinom& NOK(polinom&, polinom&);
 friend std::ostream &operator<<(std::ostream &, polinom&);
 };
 #endif //LAB_POLINOM_C_LAB_POLINOM_C_H
