7bff36e28a65d4d2bc67c8ac07cf39c4c8a3ec07
denis_adidas <veb-kamera2013@ya.ru>
2022-05-25 15:07:27 +0300

    Update research_lib.h

--- task_from_Turlykov/research_lib.h
+++ task_from_Turlykov/research_lib.h
@@ -9,9 +9,13 @@ void ltoae(long num, char str[]);
 long my_strtol(char* str, char** pEnd, int ss);
 char* my_string();
 void* resize(void* mas);
-float* masFloat(char* str);
+int* masFloat(char* str);
 int mas_len(float* mas);
 int my_pow(int ss, int count);
 int min(float* mas);
-
+int** my_square_matrix_pow(int** matrix, int size, int grade);
+int **matrix_mylti(int **matrix, int size);
+int** make_matrix_from_vector(int* vector, int size);
+void print_matrix(int** matrix, int size);
+int* from_number_to_vector(int n, int degree);
 #endif
